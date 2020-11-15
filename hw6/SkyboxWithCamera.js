e4f2f9b990068d63cad52fffb81b4a7132956b3d
Thomas Powell <tkpowell@iastate.edu>
2020-11-15 12:44:46 -0600

    Files

---
+++ hw6/SkyboxWithCamera.js
@@ -0,0 +1,148 @@
+//
+// Skybox using Three.js.
+//
+
+var path = "../images/winter/winterskyday";
+//var path = "../images/stars/Stargate";
+////var path = "../images/sky/";
+var imageNames = [
+                  path + "lf.bmp",
+                  path + "rt.bmp",
+                  path + "up.bmp",
+                  path + "dn.bmp",
+                  path + "ft.bmp",
+                  path + "bk.bmp"
+                  ];
+
+
+var axis = 'z';
+var paused = false;
+var camera;
+
+//translate keypress events to strings
+//from http://javascript.info/tutorial/keyboard-events
+function getChar(event) {
+if (event.which == null) {
+ return String.fromCharCode(event.keyCode) // IE
+} else if (event.which!=0 && event.charCode!=0) {
+ return String.fromCharCode(event.which)   // the rest
+} else {
+ return null // special key
+}
+}
+
+function cameraControl(c, ch)
+{
+  var distance = c.position.length();
+  var q, q2;
+
+  switch (ch)
+  {
+  // camera controls
+  case 'w':
+    c.translateZ(-0.1);
+    return true;
+  case 'a':
+    c.translateX(-0.1);
+    return true;
+  case 's':
+    c.translateZ(0.1);
+    return true;
+  case 'd':
+    c.translateX(0.1);
+    return true;
+  case 'r':
+    c.translateY(0.1);
+    return true;
+  case 'f':
+    c.translateY(-0.1);
+    return true;
+  case 'j':
+    // need to do extrinsic rotation about world y axis, so multiply camera's quaternion
+    // on left
+    q = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0),  5 * Math.PI / 180);
+    c.applyQuaternion(q);
+    return true;
+  case 'l':
+    q = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0),  -5 * Math.PI / 180);
+    c.applyQuaternion(q);
+    return true;
+  case 'i':
+    // intrinsic rotation about camera's x-axis
+    c.rotateX(5 * Math.PI / 180);
+    return true;
+  case 'k':
+    c.rotateX(-5 * Math.PI / 180);
+    return true;
+  case 'O':
+    c.lookAt(new THREE.Vector3(0, 0, 0));
+    return true;
+  case 'S':
+    c.fov = Math.min(80, c.fov + 5);
+    c.updateProjectionMatrix();
+    return true;
+  case 'W':
+    c.fov = Math.max(5, c.fov  - 5);
+    c.updateProjectionMatrix();
+    return true;
+
+    // alternates for arrow keys
+  case 'J':
+    //this.orbitLeft(5, distance)
+    c.translateZ(-distance);
+    q = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0),  5 * Math.PI / 180);
+    c.applyQuaternion(q);
+    c.translateZ(distance);
+    return true;
+  case 'L':
+    //this.orbitRight(5, distance)
+    c.translateZ(-distance);    
+    q = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0),  -5 * Math.PI / 180);
+    c.applyQuaternion(q);
+    c.translateZ(distance);
+    return true;
+  case 'I':
+    //this.orbitUp(5, distance)
+    c.translateZ(-distance);
+    c.rotateX(-5 * Math.PI / 180);
+    c.translateZ(distance);
+    return true;
+  case 'K':
+    //this.orbitDown(5, distance)
+    c.translateZ(-distance);
+    c.rotateX(5 * Math.PI / 180);
+    c.translateZ(distance);
+    return true;
+  }
+  return false;
+}
+
+function handleKeyPress(event)
+{
+  var ch = getChar(event);
+  if (cameraControl(camera, ch)) return;
+}
+
+function start()
+{
+  window.onkeypress = handleKeyPress;
+
+  var scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 45, 1.5, 0.1, 1000 );
+
+  var ourCanvas = document.getElementById('theCanvas');
+  var renderer = new THREE.WebGLRenderer({canvas: ourCanvas});
+
+  // load the six images
+  var ourCubeMap = new THREE.CubeTextureLoader().load( imageNames );
+
+  // this is too easy, don't need a mesh or anything
+  scene.background = ourCubeMap;
+
+  var render = function () {
+    requestAnimationFrame( render );
+     renderer.render(scene, camera);
+  };
+
+  render();
+}
